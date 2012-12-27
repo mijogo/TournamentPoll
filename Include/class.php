709a05dd7ae4fa935da01cfae1bcb70c8cde7505
Michel Jofre <michel.jofreg@gmail.com>
2012-12-26 23:12:35 -0300

    Se hacen modificaciones

---
+++ Include/class.php
@@ -0,0 +1,5 @@
+<?php
+include_once "DataBase.php";
+include_once "Personaje.php";
+include_once "PersonajeBD.php"
+?>
---
+++ Include/include.php
@@ -0,0 +1,8 @@
+<?php
+require_once "config.php";
+include_once "class.php";
+include_once "function.php";
+include_once "logicC.php";
+include_once "logicV.php";
+include_once "structura.php";
+?>
